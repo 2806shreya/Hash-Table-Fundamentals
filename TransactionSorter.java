166798643785d97bdc02e6bd8fc87785b7218e99
Shreya Kulkarni <sk0262@srmist.edu.in>
2026-04-02 13:49:15 +0530

    Added 6 sorting and searching problems

---
+++ TransactionSorter.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Transaction {
+    String id;
+    double fee;
+    String timestamp;
+
+    Transaction(String id, double fee, String timestamp) {
+        this.id = id;
+        this.fee = fee;
+        this.timestamp = timestamp;
+    }
+}
+
+public class TransactionSorter {
+
+    // Bubble Sort (by fee)
+    public static void bubbleSort(List<Transaction> list) {
+        int n = list.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).fee > list.get(j + 1).fee) {
+                    Collections.swap(list, j, j + 1);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    // Insertion Sort (fee + timestamp)
+    public static void insertionSort(List<Transaction> list) {
+        for (int i = 1; i < list.size(); i++) {
+            Transaction key = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && list.get(j).fee > key.fee) {
+                list.set(j + 1, list.get(j));
+                j--;
+            }
+
+            list.set(j + 1, key);
+        }
+    }
+
+    public static void print(List<Transaction> list) {
+        for (Transaction t : list) {
+            System.out.println(t.id + " : " + t.fee + " @ " + t.timestamp);
+        }
+    }
+
+    public static void highFees(List<Transaction> list) {
+        System.out.println("High Fee Transactions:");
+        for (Transaction t : list) {
+            if (t.fee > 50) {
+                System.out.println(t.id);
+            }
+        }
+    }
+}
